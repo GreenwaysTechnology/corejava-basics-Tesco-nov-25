8c94d0af3ab648ad94cad81cdb945623b453f6e7
subugee <sasubramanian_md@hotmail.com>
2025-11-25 17:42:52 +0530

    oop and java

---
+++ OOApp/src/main/java/Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    //instance variables - state
+    int id = 1;
+    String name = "Subramanian Murugan";
+    String address = "Coimbatore, Tamil Nadu, India";
+    String email = "sasubramanian_md@hotmail.com";
+    String phoneNo = "9003706368";
+    String SSN = "232323232323132213";
+    double amount = 1000000;
+    String accountType = "Current Account";
+}
---
+++ OOApp/src/main/java/CustomerMain.java
@@ -0,0 +1,16 @@
+public class CustomerMain {
+    public static void main(String[] args) {
+        //create instance/object from the class
+        Customer customer = new Customer();
+        System.out.println("Id :" + customer.id);
+        System.out.println("Name :" + customer.name);
+        System.out.println("Address :" + customer.address);
+        System.out.println("Email :" + customer.email);
+        System.out.println("Phone  :" + customer.phoneNo);
+        System.out.println("SSN :" + customer.SSN);
+        System.out.println("Amount :" + customer.amount);
+        System.out.println("Account Type :" + customer.accountType);
+
+
+    }
+}
